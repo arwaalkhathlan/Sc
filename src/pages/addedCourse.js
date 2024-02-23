99ec4772a4279063dcb42b37705b9d1931cf70cb
ferhad_Baaqieel <ferhad.baaqieel@gmail.com>
2024-02-23 14:01:25 +0300

    fix button color in addedCourse.js

--- src/pages/addedCourse.js
+++ src/pages/addedCourse.js
@@ -21,7 +21,7 @@ const AddedCourse = () => {
               <div className="col-7">
                 <h3>Advanced Styling with Responsive Design</h3>
 
-                <DropdownButton style={{ marginBottom: '40px', marginTop:'60px'}} id="dropdown-basic-button" title="Assessments ">
+                <DropdownButton variant="custom"style={{ marginBottom: '40px', marginTop:'60px'}} id="dropdown-basic-button" title="Assessments ">
                   <Dropdown.Item href="#/action-1">Week 2: Create a Responsive Stylesheet</Dropdown.Item>
                   <Dropdown.Item href="#/action-2">week 4: final project photo gallery  </Dropdown.Item>
                 </DropdownButton>
